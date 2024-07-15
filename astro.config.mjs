a8f77771d2da30218a431426ec3aa65dd12548df
Dominik Germek <ido3f@de.bosch.com>
2024-07-15 14:42:46 +0200

    Fixed path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
+  site: 'https://sehas-workshop.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
